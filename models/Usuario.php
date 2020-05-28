260774d5b7c2d4e7f2ad4f4d0800409ec55c53e0
Moisés Costa <moisesmengo@live.com>
2020-05-28 15:21:39 -0300

    migrando para orientaçao a objetos

---
+++ models/Usuario.php
@@ -0,0 +1,43 @@
+<?php
+    class Usuario {
+        private $id;
+        private $nome;
+        private $divida;
+        private $cidade;
+
+        public function  getId(){
+            return $this->id;
+        }
+        public function setId($id){
+            $this->id = trim($id);
+        }
+
+        public function  getNome(){
+            return $this->nome;
+        }
+        public function setNome($nome){
+            $this->nome = ucwords(trim($nome));
+        }
+
+        public function  getDivida(){
+            return $this->divida;
+        }
+        public function setEmail($divida){
+            $this->divida = trim($divida);
+        }
+
+        public function  getCidade(){
+            return $this->cidade;
+        }
+        public function setCidade($cidade){
+            $this->cidade = ucwords(trim($cidade));
+        }
+    }
+
+    interface UsuarioDAO{
+        public function add(Usuario $u);
+        public function findAll();
+        public function findById($id);
+        public function update(Usuario $u);
+        public function delete($id);
+    }
